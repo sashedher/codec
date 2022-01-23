29b5b030931d98130ac119d91e3da1d0252c7d68
sashedher <sashedher.14@gmail.com>
2022-01-23 23:49:02 +0530

    DIV 1629

--- Code_forces/1629_Download_More_Ram.cpp
+++ Code_forces/1629_Download_More_Ram.cpp
@@ -1,36 +1,34 @@
 // https://codeforces.com/contest/1629/problem/A  Inprocess
 #include<bits/stdc++.h>
 using namespace std;
+ void solve(){
 
-bool comp(pair<long long int,long long int> x,pair<long long int,long long int> y){
-    return x.first<y.first;
-}
+    
+    int n,k;
+    cin>>n>>k;
+    vector<pair<int, int>> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i].first;
+    }     
+    for(int i=0;i<n;i++){
+        cin>>a[i].second;
+    }    
+    sort(a.begin(),a.end());
+    
+    for(int i=0;i<n;i++){
+        if(a[i].first<=k){
+            k=k+a[i].second;
+        }
+    } 
+    cout<<k<<"\n";
+ }
+
 
 int main(){
-    long long int t;
+    int t;
     cin>>t;
     while(t--){
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); 
-        
-        long long int n,k=0;
-        cin>>n>>k;
-        vector<pair<long long int,long long int>> a(n);
-        for(long long int i=0;i<n;i++){
-            cin>>a[i].first;
-        }     
-        for(long long int i=0;i<n;i++){
-            cin>>a[i].second;
-        }    
-        sort(a.begin(),a.end(),comp);
-        
-        for(long long int i=0;i<n;i++){
-          if(a[i].first<=k){
-              k=k+a[i].second;
-            }
-          else break;
-        } 
-        cout<<k<<" "<<t+1<<"\n";
+       solve();
     }
     
     return 0;
