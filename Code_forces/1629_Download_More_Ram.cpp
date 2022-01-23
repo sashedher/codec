af00a5698a2fcc5d5909c311f08bc90214cfb9fa
sashedher <sashedher.14@gmail.com>
2022-01-24 00:04:06 +0530

    DIV 1629 A Solved

--- Code_forces/1629_Download_More_Ram.cpp
+++ Code_forces/1629_Download_More_Ram.cpp
@@ -1,34 +1,33 @@
 // https://codeforces.com/contest/1629/problem/A  Inprocess
 #include<bits/stdc++.h>
 using namespace std;
- void solve(){
 
-    
-    int n,k;
-    cin>>n>>k;
-    vector<pair<int, int>> a(n);
-    for(int i=0;i<n;i++){
-        cin>>a[i].first;
-    }     
-    for(int i=0;i<n;i++){
-        cin>>a[i].second;
-    }    
-    sort(a.begin(),a.end());
-    
-    for(int i=0;i<n;i++){
-        if(a[i].first<=k){
-            k=k+a[i].second;
-        }
-    } 
-    cout<<k<<"\n";
- }
-
+bool comp(pair<int,int> x,pair<int,int> y){
+    return x.first<y.first;
+}
 
 int main(){
     int t;
     cin>>t;
-    while(t--){
-       solve();
+    while(t--){  
+        int n,k;
+        cin>>n>>k;
+        vector<pair<int,int>> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i].first;
+        }     
+        for(int i=0;i<n;i++){
+            cin>>a[i].second;
+        }    
+        sort(a.begin(),a.end(),comp);
+        
+        for(int i=0;i<n;i++){
+          if(a[i].first<=k){
+              k+=a[i].second;
+            }
+          else break;
+        } 
+        cout<<k<<"\n";
     }
     
     return 0;
