67c1638fc208df89a22ca68ccb0e19c4c4d4aa80
sashedher <sashedher.14@gmail.com>
2022-02-06 23:59:59 +0530

    leet code

---
+++ Leetcode/twosum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> op;
+        vector<int> res(2);
+        for(int i=0;i<nums.size();i++){
+            op.insert({nums[i],i});
+        }
+        for(int i=0;i<nums.size();i++){
+            if(op.find(target-nums[i])!=op.end() && i!=op.find(abs(target-nums[i]))->second){
+                res[0]=i;
+                res[1]=op.find(target-nums[i])->second;
+                   break;
+            }
+        }
+        return res;
+    }
+};
