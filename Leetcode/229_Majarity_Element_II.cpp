a966856e8e5edddaabf11aaaddbc0d6b92531718
sashedher <sashedher.14@gmail.com>
2022-02-10 20:09:55 +0530

    using boyer-moore algorithm

--- Leetcode/229_Majarity_Element_II.cpp
+++ Leetcode/229_Majarity_Element_II.cpp
@@ -1,21 +1,35 @@
-// https://leetcode.com/problems/majority-element-ii/ -Accepted TC : O(n), SC : O(n)
+// https://leetcode.com/problems/majority-element-ii/ -Accepted TC : O(n), SC : O(1)
 #include<bits/stdc++.h>
 using namespace std;
 
+// TC : O(n), SC : O(1) using Boyer-Moore  voting Algorithm
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        multiset<int> mp(nums.begin(),nums.end());
+        int c1=0,c2=0,e1=0,e2=0;
         vector<int> res;
-        set<int> ans(nums.begin(),nums.end());
-        for(auto it=ans.begin();it!=ans.end();it++){
-            if(mp.count(*it)>nums.size()/3){
-                res.push_back(*it);
-            }
+        for(int a:nums){
+            if(e1==a) c1++;
+            else if(e2==a) c2++;
+            else if(c1==0){e1=a;c1=1;}
+            else if(c2==0){e2=a;c2=1;}
+            else{c1--;c2--;}
         }
+        int c11=0,c22=0;
+        for(int a:nums){
+            if(a==e1) c11++;
+            else if(a==e2) c22++;
+        }
+        if(c11>nums.size()/3) res.push_back(e1);
+        if(c22>nums.size()/3) res.push_back(e2);
+        
         return res;
     }
+    
 };
+
+
+
 
 int main(){
     Solution ob;
