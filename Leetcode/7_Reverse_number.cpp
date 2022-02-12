152343f7c767c178c1ced51078bda684bca76f7b
sashedher <sashedher.14@gmail.com>
2022-02-12 23:13:40 +0530

    reverse number

---
+++ Leetcode/7_Reverse_number.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/reverse-integer/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mx 2147483647
+#define mn -2147483648
+class Solution {
+public:
+    int reverse(int x) {
+        long long int rev=0,r=0,t;
+        t=abs(x);
+        
+      
+        while(t>0){
+            r=t%10;
+            if(rev<=mx/10) rev=rev*10+r;
+            else return 0;
+            t/=10;
+            
+        }
+        if(x>=0)  return rev;
+        else return -rev;
+    }
+};
