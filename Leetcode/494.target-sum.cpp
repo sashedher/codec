56423bcea510f8715f503eea2bd3af1c41746047
sashedher <sashedher.14@gmail.com>
2022-02-17 12:20:52 +0530

    Run SubSetSum on (Nums,target+sum/2) and from j=0 not from j=1 to count ele 0's

---
+++ Leetcode/494.target-sum.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=494 lang=cpp
+ *
+ * [494] Target Sum
+ */
+
+// @lc code=start
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        target=abs(target);
+        int sm=0;       
+        for(int i=0;i<nums.size();i++){
+            sm+=nums[i];
+        }
+        if((target+sm)%2!=0 || target>sm) return 0;
+        return Countsubsetsum(nums,(target+sm)/2);
+        
+    }
+    int Countsubsetsum(vector<int>& nums,int c){
+        vector<vector<int>> t(nums.size()+1,vector<int>(c+1,0));
+        for(int i=0;i<=nums.size();i++){
+            t[i][0]=1;
+        }
+        for(int i=1;i<=nums.size();i++){
+            for(int j=0;j<=c;j++){            // count from j=0 because if an array element contains zero  that sould be counted
+                if(nums[i-1]<=j){
+                    t[i][j]=t[i-1][j]+t[i-1][j-nums[i-1]];
+                }
+                else t[i][j]=t[i-1][j];
+                
+            }
+        }
+        return t[nums.size()][c];
+    }
+};
+// @lc code=end
+
