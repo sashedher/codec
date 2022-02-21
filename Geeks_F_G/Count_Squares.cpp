d0e46676867fe68fef9b513ccb8cf14b327eeac6
sashedher <sashedher.14@gmail.com>
2022-02-21 23:24:25 +0530

    Count Squares

---
+++ Geeks_F_G/Count_Squares.cpp
@@ -0,0 +1,33 @@
+// https://practice.geeksforgeeks.org/problems/count-squares3649/1
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        int i=1;
+        while(i*i<N){
+            i++;
+        }
+        return i-1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
