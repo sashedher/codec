455f5c2f49cd6218e74827da0bafbbee4e652c60
sashedher <sashedher.14@gmail.com>
2022-02-22 20:09:09 +0530

    lcs

--- Interview_bit/Dynamic Programming/Longest_Common_SubSequence.cpp
+++ Interview_bit/Dynamic Programming/Longest_Common_SubSequence.cpp
@@ -1,18 +1,12 @@
-/*
- * @lc app=leetcode id=1143 lang=cpp
- *
- * [1143] Longest Common Subsequence
- */
-
-// @lc code=start
-
+// https://www.interviewbit.com/problems/longest-common-subsequence/
+ 
 #include<bits/stdc++.h>
 using namespace std;
  
-class Solution {
-public:
-    int longestCommonSubsequence(string t1, string t2) {
-        vector<vector<int>> t(t1.length()+1,vector<int>(t2.length()+1,0));
+class Solution{
+public: 
+    int solve(string t1, string t2) {
+    vector<vector<int>> t(t1.length()+1,vector<int>(t2.length()+1,0));
         for(int i=1;i<=t1.length();i++){
             for(int j=1;j<=t2.length();j++){
                 if(t1[i-1]==t2[j-1])
@@ -23,5 +17,3 @@ public:
         return t[t1.length()][t2.length()]; 
     }
 };
-// @lc code=end
-
