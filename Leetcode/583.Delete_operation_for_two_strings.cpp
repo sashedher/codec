447597226903c86338dd34c05dbd29f1d1049c82
sashedher <sashedher.14@gmail.com>
2022-03-02 08:02:27 +0530

    SMA Edit-distance,don't count for replace opern

---
+++ Leetcode/583.Delete_operation_for_two_strings.cpp
@@ -0,0 +1,26 @@
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string t1, string t2) {
+        vector<vector<int>> t(t1.length()+1,vector<int>(t2.length()+1,0));
+        for(int i=0;i<=t1.length();i++){
+            t[i][0]=i;
+        }
+        for(int j=0;j<=t2.length();j++){
+            t[0][j]=j;
+        }
+        for(int i=1;i<=t1.length();i++){
+            for(int j=1;j<=t2.length();j++){
+                if(t1[i-1]==t2[j-1])
+                    t[i][j]=t[i-1][j-1];
+                else t[i][j]=1+min( { t[i-1][j],  t[i][j-1]}  );
+            }
+        }
+        return t[t1.length()][t2.length()];
+        
+    }
+};
