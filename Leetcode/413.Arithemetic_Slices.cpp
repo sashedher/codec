09ccceb747ca1949a64f3f5bf1ba55cbca9eec68
sashedher <sashedher.14@gmail.com>
2022-03-04 10:40:04 +0530

    change in expression for consecutive sum

--- Leetcode/413.Arithemetic_Slices.cpp
+++ Leetcode/413.Arithemetic_Slices.cpp
@@ -8,17 +8,13 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int c=0,s=0;
         if(nums.size()<3) return 0;
-        
+             
         for(int i=0;i<nums.size()-2;i++){
-            if(nums[i+1]-nums[i]== nums[i+2]-nums[i+1])  c+=1;
-            else {
-                s=s+(c*(c+1))/2;
+            if(nums[i+1]-nums[i]== nums[i+2]-nums[i+1])  s = s + ++c;
+            else 
                 c=0;
-            }
-        }
-        
-        s=s+(c*(c+1))/2;      
-        
+        } 
+                
         return s;
     }
 };
