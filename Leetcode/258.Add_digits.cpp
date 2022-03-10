295da25215a7d5ac17a819f2b23c93e878121138
sashedher <sashedher.14@gmail.com>
2022-03-10 12:32:16 +0530

    using digital root O(1) time

--- Leetcode/258.Add_digits.cpp
+++ Leetcode/258.Add_digits.cpp
@@ -6,13 +6,7 @@ using namespace std;
 class Solution {
 public:
     int addDigits(int num) {
-        int s=0;
-        while(num>0){
-            s=s+num%10;
-            num/=10;
-        }
-         int r=s%10 + (s/10)%10 + (s/100)%10 + (s/1000)%10;
-        r=r%10+(r/10)%10;
-        return r;
+        if(num==0) return 0;
+        else return 1+(num-1) %9;
     }
 };
