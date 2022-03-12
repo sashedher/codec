eed2a1ab279c92e4e9e85d1241dfb9447e28a5c5
sashedher <sashedher.14@gmail.com>
2022-03-12 17:37:44 +0530

    traverse 2 lists circularly(h1->h2 and h2->h1) until both are same

--- Leetcode/160.Intersection_of_two_linked_Lists.cpp
+++ Leetcode/160.Intersection_of_two_linked_Lists.cpp
@@ -13,33 +13,10 @@ using namespace std;
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int u=0,l=0;
         ListNode *l1=headA,*l2=headB;
-        while(l1){
-            u++;
-            l1=l1->next;
-        }
-        while(l2){
-            l++;
-            l2=l2->next;
-        }
-        l1=headA;
-        l2=headB;
-        if(u>l){
-            int d=u-l;
-            while(d--){
-                l1=l1->next;        
-            }
-        }
-        if(l>u){
-            int d=l-u;
-            while(d--){
-                l2=l2->next;        
-            }
-        }
         while(l1!=l2){
-            l1=l1->next;
-            l2=l2->next;
+            l1=!l1?headB:l1->next;
+            l2=!l2?headA:l2->next;
         }
         return l1;
         
