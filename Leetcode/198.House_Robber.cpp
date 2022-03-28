e9327dc5f10bde809df51623be5193ed88511c1d
sashedher <sashedher.14@gmail.com>
2022-03-28 23:34:39 +0530

    max( nums(x)+dp(x-2) , dp(x-1))

---
+++ Leetcode/198.House_Robber.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/house-robber/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> t(n+1,0);
+        t[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            t[i]=max(t[i-1],t[i-2]+nums[i-1]);
+        }
+        return t[n];
+    }
+};
