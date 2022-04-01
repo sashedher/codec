b5b67182a4e589e243ddc2fc62156b2cb18b3255
sashedher <sashedher.14@gmail.com>
2022-04-01 22:58:01 +0530

    LCS -> string len == subseq length

---
+++ Leetcode/392.Is_subsequence.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/is-subsequence/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string t1, string t2) {
+        vector<vector<int>> t(t1.length()+1,vector<int>(t2.length()+1,0));
+        for(int i=1;i<=t1.length();i++){
+            for(int j=1;j<=t2.length();j++){
+                if(t1[i-1]==t2[j-1])
+                    t[i][j]=1+t[i-1][j-1];
+                else t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
+        }
+        if(t1.length()==t[t1.length()][t2.length()]) return true;
+        else return false;
+        
+    }
+};
