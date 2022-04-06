8a8bec756173a7e1a9dce9bae91aee8226760b66
sashedher <sashedher.14@gmail.com>
2022-04-06 19:22:31 +0530

    use fast and slow pointer approach

--- Leetcode/876.Middle_of_the_Linked_List.cpp
+++ Leetcode/876.Middle_of_the_Linked_List.cpp
@@ -15,20 +15,15 @@ using namespace std;
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int t=0;
-        ListNode* t1=head;
-        while(t1){
-            t++;
-            t1=t1->next;
-        }
-        if(t==1) return head;
-        t=t/2;
-        
-        t1=head;
-        while(t--){
-            t1=t1->next;
-        }
+         
+        ListNode* t1=head,*t2=head;
+        while(t2){
+            t2=t2->next;
+            if(t2){
+                t2=t2->next;
+                t1=t1->next;
+            }
+        } 
         return t1;
     }
-    
 };
