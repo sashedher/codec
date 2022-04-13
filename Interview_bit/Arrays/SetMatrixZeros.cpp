3b5fe89ba388dda8e319cf9051fbbf36ee593185
sashedher <sashedher.14@gmail.com>
2022-04-13 16:50:26 +0530

    O(1) space use row, column headers

--- Interview_bit/Arrays/SetMatrixZeros.cpp
+++ Interview_bit/Arrays/SetMatrixZeros.cpp
@@ -8,28 +8,47 @@ class Solution{
         // Do not read input, instead use the arguments to the function.
         // Do not print the output, instead return values as specified
         // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-        set<int> row,col;
-        for(int i=0;i<A.size();i++){
-            for(int j=0;j<A[i].size();j++){
-                if(A[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+        int r=1,c=1;
+        int m=A.size();
+        int n= A[0].size();
+        for (int i = 0; i <m; i++){
+            if(A[i][0]==0){
+                c=0;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++){
+            if(A[0][i]==0){
+                r=0;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < m; i++){
+            
+            for (int j = 1; j < n; j++){
+                
+                if (A[i][j] == 0){
+                    A[0][j]=0;
+                    A[i][0]=0;
                 }
             }
         }
-        for(auto it=row.begin();it!=row.end();it++){
-            int r=*it;
-            for(int j=0;j<A[r].size();j++){
-                A[r][j]=0;
+        for (int i = 1; i < m; i++){
+            
+            for (int j = 1; j < n; j++){
+                
+                if (A[i][0]==0 || A[0][j]==0)   A[i][j]=0;
+                
             }
         }
-        for(auto it=col.begin();it!=col.end();it++){
-            int c=*it;
-            for(int j=0;j<A.size();j++){
-                A[j][c]=0;
-            }
+        
+        if(c==0){
+            for (int i = 0; i < m; i++)  A[i][0]=0;
         }
-
+        if(r==0){
+            for (int i = 0; i < n; i++)  A[0][i]=0;
+        }
     }
 };
 
