b4ae2d8dc199c4cc4e0768db974893f0108b9094
sashedher <sashedher.14@gmail.com>
2022-04-14 11:52:46 +0530

    O(1) space use row, column headers

--- Leetcode/73.Set_Matrix_Zeroes.cpp
+++ Leetcode/73.Set_Matrix_Zeroes.cpp
@@ -6,27 +6,46 @@ using namespace std;
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& A) {
-        set<int> row, col;
-        for (int i = 0; i < A.size(); i++)
-        {
-            for (int j = 0; j < A[i].size(); j++)
-            {
-                if (A[i][j] == 0)
-                {
-                    row.insert(i);
-                    col.insert(j);
+        int r=1,c=1;
+        int m=A.size();
+        int n= A[0].size();
+        for (int i = 0; i <m; i++){
+            if(A[i][0]==0){
+                c=0;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++){
+            if(A[0][i]==0){
+                r=0;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < m; i++){
+            
+            for (int j = 1; j < n; j++){
+                
+                if (A[i][j] == 0){
+                    A[0][j]=0;
+                    A[i][0]=0;
                 }
             }
         }
-        for (int i = 0; i < A.size(); i++)
-        {
-            for (int j = 0; j < A[i].size(); j++)
-            {
-                if (row.find(i)!=row.end() || col.find(j)!=col.end())
-                {
-                    A[i][j]=0;
-                }
+        for (int i = 1; i < m; i++){
+            
+            for (int j = 1; j < n; j++){
+                
+                if (A[i][0]==0 || A[0][j]==0)   A[i][j]=0;
+                
             }
-        }       
+        }
+        
+        if(c==0){
+            for (int i = 0; i < m; i++)  A[i][0]=0;
+        }
+        if(r==0){
+            for (int i = 0; i < n; i++)  A[0][i]=0;
+        }
     }
 };
