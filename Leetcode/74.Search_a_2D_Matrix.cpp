df7769edada9912e8da8cbf96c03afa1f214f945
sashedher <sashedher.14@gmail.com>
2022-04-30 20:09:39 +0530

    Apply binary search along 1st column and then binary search on selected row

---
+++ Leetcode/74.Search_a_2D_Matrix.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/search-a-2d-matrix
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool BinSer(vector<int> &mat,int target){
+        int l=0,h=mat.size()-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(mat[m]==target) return true;
+            else if(mat[m]>target) h=m-1;
+            else l=m+1;
+            
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int l=0,h=n-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(BinSer(matrix[m],target)) return true;
+            else if(matrix[m][0]>target)    h=m-1;
+            else if(matrix[m][0]<target)    l=m+1;
+        }
+        return false;
+    }
+};
