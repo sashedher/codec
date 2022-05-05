4eeb8576574ac0b65c0d6ef15072a5cb18febfcf
sashedher <sashedher.14@gmail.com>
2022-05-05 20:14:10 +0530

    try to do it in conventional manner

---
+++ Leetcode/138.Copy_List_With_Random_Pointer.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/copy-list-with-random-pointer
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        return head;
+    }
+};
