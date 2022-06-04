ac0c7f3b81ac20caad7761d83a9bb33231b34a02
sashedher <sashedher.14@gmail.com>
2022-06-04 22:51:41 +0530

    simple

---
+++ Leetcode/88.Merge_Sorted_Array.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/merge-sorted-array/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> res;
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                res.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i<m){
+            res.push_back(nums1[i]);
+                i++;
+        }
+        while(j<n){
+            res.push_back(nums2[j]);
+                j++;
+        }
+//         for(int x:res){
+//             cout<<x<<" ";
+
+//         }
+        nums1.clear();
+        nums1=res;
+    }
+};
