e254fe60a9d07b982deaa10a05c30218b4e42c45
sashedher <sashedher.14@gmail.com>
2022-08-13 10:16:54 +0530

    compare 2 strings increment i only when characters are equal return i==n1

--- Leetcode/392.Is_subsequence.cpp
+++ Leetcode/392.Is_subsequence.cpp
@@ -6,16 +6,12 @@ using namespace std;
 class Solution {
 public:
     bool isSubsequence(string t1, string t2) {
-        vector<vector<int>> t(t1.length()+1,vector<int>(t2.length()+1,0));
-        for(int i=1;i<=t1.length();i++){
-            for(int j=1;j<=t2.length();j++){
-                if(t1[i-1]==t2[j-1])
-                    t[i][j]=1+t[i-1][j-1];
-                else t[i][j]=max(t[i-1][j],t[i][j-1]);
-            }
+        int n1=t1.length(),n2=t2.length();
+        int i=0,j=0;
+        while(i<n1 && j<n2){
+            if(t1[i]==t2[j]) i++;
+            j++;
         }
-        if(t1.length()==t[t1.length()][t2.length()]) return true;
-        else return false;
-        
+        return i==n1;
     }
 };
