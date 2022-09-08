bd6e635307ad3b882a9d657a0354707806ba53f2
sashedher <sashedher.14@gmail.com>
2022-09-08 11:00:59 +0530

    matrix simulation

---
+++ Interview_bit/Arrays/Spiral_Order_Matrix_II.cpp
@@ -0,0 +1,36 @@
+// https://www.interviewbit.com/problems/spiral-order-matrix-ii/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public: 
+    vector<vector<int> > generateMatrix(int n) {
+        vector<vector<int>> res(n,vector<int>(n,0));
+            
+            int l=0;
+            int r=n-1;
+            int u=0;
+            int d=n-1;
+            int c=1;
+            while(l<=r && u<=d){
+                
+                for(int i=l;i<=r;i++) res[u][i]=c++;
+                u++;
+                if(u>d) break;
+                
+                for(int i=u;i<=d;i++) res[i][r]=c++;
+                r--;
+                if(r<l) break;
+                
+                for(int i=r;i>=l;i--) res[d][i]=c++;
+                d--;
+                if(d<u) break;
+                
+                for(int i=d;i>=u;i--) res[i][l]=c++;
+                l++;
+                if(l>r) break;
+            }
+        return res; 
+    }
+};
