a16d6176b5d786f32f889fdb0ddcd306086ef8ac
sashedher <sashedher.14@gmail.com>
2022-09-14 23:07:13 +0530

    bottom up dp similar to 198-leetcode

--- Coding_Ninjas/Maximum_sum_of_non-adjacent_elements.cpp
+++ Coding_Ninjas/Maximum_sum_of_non-adjacent_elements.cpp
@@ -10,5 +10,11 @@ int rec(vector<int> & v, int i){
 
 int maximumNonAdjacentSum(vector<int> &nums){
     int n=nums.size();
-    return rec(nums,nums.size()-1);
+    vector<int> dp(n,0);
+    dp[0]=nums[0];
+    dp[1]=max(nums[0],nums[1]);
+    for(int i=2;i<n;i++){
+         dp[i]=max(nums[i]+dp[i-2], dp[i-1]);
+    }
+    return dp[n-1];
 }
