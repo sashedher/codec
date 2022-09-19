47892d1b1c7bbc0635bf09b141a300e2d0b2a45f
sashedher <sashedher.14@gmail.com>
2022-09-19 22:49:34 +0530

    same as Q51 and return res.size()

---
+++ Leetcode/52.N-Queens_II.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/n-queens-ii/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string> &ans,int n){
+        for(int i=col;i>=0;i--){
+            if(ans[row][i]=='Q') return false;
+        }
+        int i=row,j=col;
+        while(i>=0 && j>=0){
+            if(ans[i][j]=='Q') return false;
+            i--,j--;
+        }
+        i=row,j=col;
+        while(i<n && j>=0){
+            if(ans[i][j]=='Q') return false;
+            i++,j--;
+        }
+        return true;
+    }
+    void backtrack(int j,int n,vector<string> &ans, vector<vector<string>>& res){
+        if(j==n){
+            res.push_back(ans);
+            return;
+        }  
+        for(int i=0;i<n;i++){
+            if(isSafe(i,j,ans,n)){
+                ans[i][j]='Q';
+                backtrack(j+1,n,ans,res);
+                ans[i][j]='.';
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+        vector<string> ans(n,string(n,'.'));
+        vector<vector<string>> res;
+        // vector<int> r={1,0,0,2,10,4,40,92,352};
+        backtrack(0,n,ans,res);
+        
+        return res.size();
+        
+    }
+};
