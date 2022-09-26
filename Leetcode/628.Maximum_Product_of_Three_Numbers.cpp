26c67757230094865914b2c43ee5767d938918fb
sashedher <sashedher.14@gmail.com>
2022-09-26 18:38:18 +0530

    min of product of 1st two +last one or last three

---
+++ Leetcode/628.Maximum_Product_of_Three_Numbers.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/maximum-product-of-three-numbers/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int x=max( nums[0]*nums[1]*nums[n-1] , nums[n-3]*nums[n-2]*nums[n-1] );
+        return x;
+    }
+};
