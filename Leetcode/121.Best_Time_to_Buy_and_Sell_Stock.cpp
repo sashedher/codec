799ba07aedd09b5366a40b6bd89ade74c15d0d58
sashedher <sashedher.14@gmail.com>
2022-09-29 18:15:44 +0530

    maintain min so far & calculate maximum difference

--- Leetcode/121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Leetcode/121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -6,13 +6,12 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int low=prices[0],maxP=0;
-        for(int i=1;i<prices.size();i++){
-            if(prices[i]<low) low=prices[i];
-            else{
-                maxP=max(maxP,prices[i]-low);
-            }
+       int n=prices.size(), res=INT_MIN,mn=INT_MAX;
+        mn=prices[0];
+        for(int i=1;i<n;i++){
+            res=max(res,prices[i]-mn);
+            mn=min(mn,prices[i]);
         }
-        return maxP;
+        return res<0?0 :res;
     }
 };
