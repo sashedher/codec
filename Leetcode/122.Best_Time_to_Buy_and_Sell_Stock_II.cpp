49cf287dc10909c1c9bf0a55b402da51f1ebe6c1
sashedher <sashedher.14@gmail.com>
2022-09-30 10:46:41 +0530

    simlar to Q121, use buy variable solve using recurssion for unlimited transactions

---
+++ Leetcode/122.Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve(vector<int> & pric, int i,bool flg, vector<vector<int>> &dp){
+        if(i==pric.size()) return 0;
+        if(dp[i][flg]!=-1) return dp[i][flg];
+        if(flg)
+            return dp[i][flg]= max(-pric[i]+solve(pric,i+1,1-flg,dp), solve(pric, i+1,flg,dp));
+        
+        else
+            return dp[i][flg]= max(pric[i]+solve(pric,i+1,1-flg,dp), solve(pric,i+1,flg,dp));
+        
+    }
+    
+    
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
+        return solve(prices,0,1,dp);
+    }
+};
