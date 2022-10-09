4a86f1090ee12146c6f5bb94493bf31c404794a4
sashedher <sashedher.14@gmail.com>
2022-10-09 14:35:17 +0530

    solved in single pass- no to intuitive

--- Leetcode/84.Largest_Rectangle_in_Histogram.cpp
+++ Leetcode/84.Largest_Rectangle_in_Histogram.cpp
@@ -7,25 +7,25 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int res=INT_MIN,n= heights.size();
-        vector<int> pse(n), nse(n);
         stack<int> st;
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-                if(st.empty()) pse[i]=0;
-                else pse[i]= st.top()+1;
-                st.push(i);
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+               int  rg=i,lg=0,h=heights[st.top()];
+                st.pop();
+                if(!st.empty()) lg=st.top()+1;
+                
+                res=max(res, (rg-lg)*h);
+            } 
+            st.push(i);
         }
-        while(!st.empty()) st.pop();
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-                if(st.empty()) nse[i]=n-1;
-                else nse[i]= st.top()-1;
-                st.push(i);
-        }
-        
-        for(int i=0;i<n;i++){
-            res=max(res,(nse[i]-pse[i]+1)*heights[i]);
-        }
+        while(!st.empty()){
+           int h=heights[st.top()],rg=n,lg=0;
+            st.pop();
+            if(!st.empty()) lg=st.top()+1;
+            
+            res=max(res, (rg-lg)*h);
+            
+        } 
         return res;
         
         
