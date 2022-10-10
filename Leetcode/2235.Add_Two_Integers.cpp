3b3554c9321dc7587c13655aa724a8b5c570b7d1
sashedher <sashedher.14@gmail.com>
2022-10-10 23:14:56 +0530

    num1+nums2

---
+++ Leetcode/2235.Add_Two_Integers.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/add-two-integers/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
