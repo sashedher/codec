f3883d181fe48aeb9edac4c5e3e08ab142eef2c2
Sashedher J <sashedher.14+github@gmail.com>
2022-10-19 21:44:47 +0530

    update  all var in every iteration and count res+= (long) max(0,min( jmin, jmax)- jbad);

---
+++ Leetcode/2444.Count_Subarrays_With_Fixed_Bounds.cpp
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/count-subarrays-with-fixed-bounds/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+       long long res=0;
+        int jmin=-1,jmax=-1,jbad=-1,n=nums.size();
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]==minK) jmin=i;
+            if(nums[i]==maxK) jmax=i;
+            if(nums[i]>maxK || nums[i]<minK) jbad=i;
+            
+            res+= (long) max(0,min(jmin,jmax)- jbad);
+        }
+        
+        return res;
+    }
+};
