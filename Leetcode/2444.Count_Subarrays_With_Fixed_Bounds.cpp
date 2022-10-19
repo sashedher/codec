7d93905c2c2932950a4229218d0dbdfadd19e7d6
Sashedher J <sashedher.14+github@gmail.com>
2022-10-19 21:48:51 +0530

    update  all var in every iteration and count res+= (long) max(0,min( jmin, jmax)- jbad);

--- Leetcode/2444.Count_Subarrays_With_Fixed_Bounds.cpp
+++ Leetcode/2444.Count_Subarrays_With_Fixed_Bounds.cpp
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/count-subarrays-with-fixed-bounds/
+// https://leetcode.com/problems/count-subarrays-with-fixed-bounds/
 
 #include<bits/stdc++.h>
 using namespace std;
