9d91e35ede9170053ce105b5fd715af1f5b32f2f
Sashedher J <sashedher.14+github@gmail.com>
2022-10-20 18:05:37 +0530

    by reducing number gradually in decreasing order

--- Leetcode/12.Integer_to_Roman.cpp
+++ Leetcode/12.Integer_to_Roman.cpp
@@ -8,57 +8,12 @@ public:
     string intToRoman(int num) {
         int n=num;
         string res="";
+        vector<pair<string,int>> mp={{"M",1000},{"CM",900},{"D",500},{"CD",400},{"C",100},{"XC",90},{"L",50},{"XL",40},{"X",10},{"IX",9},{"V",5},{"IV",4},{"I",1}};
         
-        
-       int d1,d2,d3,d4;
-        d1=num/1000;
-        d2=(num/100)%10;
-        d3=(num/10)%10;
-        d4=num%10;
-        // cout<<d1<<" "<<d2<<" "<< d3<<" "<<d4<<endl;
-        while(d1--)    res=res+'M';
-        if(d2>=5){
-            if(d2==9) res=res+"CM";
-            else{
-                d2=d2-5;
-                res=res+'D';
-                while(d2--)    res=res+'C';
-            }
-        }
-        else{
-            if(d2==4) res=res+"CD";
-            else{
-                while(d2--)    res=res+'C';
-            }
-        }
-        
-        if(d3>=5){
-            if(d3==9) res=res+"XC";
-            else{
-                d3=d3-5;
-                res=res+'L';
-                while(d3--)    res=res+'X';
-            }
-        }
-        else{
-            if(d3==4) res=res+"XL";
-            else{
-                while(d3--)    res=res+'X';
-            }
-        }
-        
-        if(d4>=5){
-            if(d4==9) res=res+"IX";
-            else{
-                d4=d4-5;
-                res=res+'V';
-                while(d4--)    res=res+'I';
-            }
-        }
-        else{
-            if(d4==4) res=res+"IV";
-            else{
-                while(d4--)    res=res+'I';
+        for(int i=0;i<13;i++){
+            while(mp[i].second<=num){
+                num-=mp[i].second;
+                res=res+mp[i].first;
             }
         }
         
