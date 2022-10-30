85c45aa1a08aa33333ca219d7833236eaf5af560
Sashedher J <sashedher.14+github@gmail.com>
2022-10-30 23:50:31 +0530

    using DisjointSet finding Noof components

--- Leetcode/1319.Number_of_Operations_to_Make_Network_Connected.cpp
+++ Leetcode/1319.Number_of_Operations_to_Make_Network_Connected.cpp
@@ -4,44 +4,75 @@
 using namespace std;
 
 
+class DisJointSet{
+public:
+    vector<int> rank,size,parent;
+    
+    DisJointSet(int n){
+        rank.resize(n,0);
+        parent.resize(n,0);
+        size.resize(n,1);
+        for(int i = 0 ;i<n; i++){
+            parent[i]=i;
+        }
+    }
+    int findParent(int u){
+        if(u==parent[u]) return u;
+        else return parent[u] = findParent(parent[u]);
+    }
+    
+    void unionRank(int u, int v){
+        int ult_u=findParent(u);
+        int ult_v=findParent(v);
+        if(ult_u == ult_v) return;
+        if(rank[ult_u] < rank[ult_v] ){
+            parent[ult_u]= ult_v;
+        }
+        if(rank[ult_v] < rank[ult_u] ){
+            parent[ult_v]= ult_u;
+        }
+        else{
+            parent[ult_v]= ult_u;
+            rank[ult_u]+=1;
+        }
+    }
+    
+    void unionSize(int u, int v){
+        int ult_u=findParent(u);
+        int ult_v=findParent(v);
+        if(ult_u == ult_v) return;
+        
+        if(size[ult_u] < size[ult_v]){
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+        }
+        else{
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v];
+        }
+        
+    }
+    
+};
+
+
 class Solution {
 public:
     
     int makeConnected(int n, vector<vector<int>>& con) {
         int e=con.size();
-        if(e<n-1) return -1;            //if number of nodes > edges(nodes-1) return -1; 
+        if(e<n-1) return -1;
         int res=0;
-        vector<vector<int>> adj(n);
-        vector<bool> vis(n,false);
+        DisJointSet ds(n);
         
-        for(int i=0;i<e;i++){
-            adj[ con[i][0] ].push_back(con[i][1]);
-            adj[con[i][1]].push_back(con[i][0]);
+        for(auto x:con) 
+            ds.unionSize(x[0],x[1]);
+        
+        
+        for(int i=0;i<ds.parent.size();i++){
+            if(ds.parent[i] == i) res++;
         }
         
-        for(int i=0;i<n;i++){                   // count number of connected components
-            
-            if(vis[i]==false){
-                res++;
-                
-                queue<int> q;
-                q.push(i);
-                vis[i]=true;
-                while(!q.empty()){
-                    int t=q.front();
-                    q.pop();
-                    for(int k=0;k<adj[t].size();k++){
-                        if(vis[ adj[t][k] ] == false ){
-                            vis[ adj[t][k] ] = true;
-                            q.push(adj[t][k]);
-                        }
-                    }
-                }
-                
-                
-            }
-            
-        }
         
         return res-1;        
     }
